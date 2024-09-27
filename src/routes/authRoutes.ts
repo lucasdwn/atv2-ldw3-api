111e8c04e743d161ed90b8cdb775376164c251f8
lucasdwn <lfcosta0804@gmail.com>
2024-09-26 23:43:25 -0300

    ✨ Feat: Nova rota para validar token.
    Rota de validação de token.

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
+import authMiddleware from '../middlewares/authMiddleware';
 
 const routes = Router();
 
 routes.post('/login', authController.login);
+routes.get("/validate-token", authMiddleware, (req, res) => {
+    res.sendStatus(200);
+});
 routes.post('/refresh-token', authController.refresh);
 
 export default routes;
