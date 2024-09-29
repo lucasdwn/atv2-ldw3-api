b2b9956b513b1730ddfc99f82cc97b6f464b4f74
lucasdwn <lfcosta0804@gmail.com>
2024-09-29 12:47:57 -0300

    🔊 Traduzindo mensagens de retorno.
    Traduzindo as mensagens de retorno antigas para portugues

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -8,27 +8,27 @@ const router = express.Router();
 router.post('/image', preserveBody('image'), async (req: Request, res: Response) => {
     try {
         if (!req.file) {
-            return res.status(400).json({ error: 'No image uploaded' });
+            return res.status(400).json({ error: 'Nenhuma imagem enviada' });
         }
         const imageUrl: IUpload = await uploadToS3(req.file, 'images');
         res.status(200).json({ imageUrl });
     } catch (error) {
-        console.error('Error uploading image:', error);
-        res.status(500).json({ error: 'Error uploading image' });
+        console.error('Erro ao carregar a imagem:', error);
+        res.status(500).json({ error: 'Erro ao carregar a imagem' });
     }
 });
 
 router.post('/document', preserveBody('document'), async (req: Request, res: Response) => {
     try {
         if (!req.file) {
-            return res.status(400).json({ error: 'No document uploaded' });
+            return res.status(400).json({ error: 'Nenhum documento enviado' });
         }
 
         const documentUrl = await uploadToS3(req.file, 'documents');
         res.status(200).json({ documentUrl });
     } catch (error) {
-        console.error('Error uploading document:', error);
-        res.status(500).json({ error: 'Error uploading document' });
+        console.error('Erro ao carregar documento:', error);
+        res.status(500).json({ error: 'Erro ao carregar documento' });
     }
 });
 
