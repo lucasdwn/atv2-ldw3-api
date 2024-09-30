c632bf78d609a6598223286cd1cba0b7b06ae28c
lucasdwn <lfcosta0804@gmail.com>
2024-09-30 18:26:17 -0300

    🚩 Add função para retornar data com horario zerado.

--- src/utils/dateService.ts
+++ src/utils/dateService.ts
@@ -5,6 +5,12 @@ class DataService {
         const offset = dateNow.getTimezoneOffset();
         return new Date(dateNow.getTime() - (offset * 60 * 1000));
     }
+
+    getDataSemHoras(date: Date): Date {
+        const newDate = new Date(date);
+        newDate.setHours(0, 0, 0, 0);
+        return newDate;
+    };
 }
 
 export default new DataService();
