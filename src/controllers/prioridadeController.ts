25f190c869b7259cf3dad7935cf5e70faf25b5be
lucasdwn <lfcosta0804@gmail.com>
2024-09-30 18:09:16 -0300

    ✨ Feat: FindOne prioridade.

--- src/controllers/prioridadeController.ts
+++ src/controllers/prioridadeController.ts
@@ -64,14 +64,26 @@ class prioridadeController {
                 return res.status(404).json({ message: 'Usuário não encontrado' });
             }
 
-            const listPrioridades = await Prioridade.find({
+            const prioridades = await Prioridade.find({
                 $or: [
                     { usuarioId: userId },
                     { usuarioId: 'admin' }
                 ]
             });
 
-            return res.status(200).json(listPrioridades);
+            const prioridadesTransformadas = prioridades.map((prioridade) => {
+                const prioridadesObj = prioridade.toObject({
+                    versionKey: false,
+                    transform: (doc, ret) => {
+                        ret.id = ret._id;
+                        delete ret._id;
+                        return ret;
+                    }
+                });
+                return prioridadesObj;
+            });
+
+            return res.status(200).json(prioridadesTransformadas);
         } catch (error: any) {
             return res.status(500).json({ message: 'Erro ao listar prioridades', error: error.message });
         }
@@ -153,7 +165,40 @@ class prioridadeController {
         } catch (error: any) {
             return res.status(500).json({ message: 'Erro ao deletar prioridade', error: error.message });
         }
-    }
+    };
+
+    public async findOnePrioridade(req: Request, res: Response): Promise<Response> {
+        try {
+            const { userId } = req.body;
+            const { prioridadeId } = req.params;
+
+            const usuario = await Usuario.findById(userId);
+
+            if (!usuario) {
+                return res.status(404).json({ message: 'Usuário não encontrado' });
+            }
+
+            const prioridade = await Prioridade.findOne({ _id: prioridadeId, usuarioId: userId });
+
+            if (!prioridade) {
+                return res.status(404).json({ message: 'Propriedade não encontrada' });
+            }
+
+            const prioridadeObj = prioridade.toObject({
+                versionKey: false,
+                transform: (doc, ret) => {
+                    ret.id = ret._id;
+                    delete ret._id;
+                    return ret;
+                }
+            });
+
+            return res.status(200).json(prioridadeObj);
+        } catch (error: any) {
+            return res.status(500).json({ message: 'Erro ao listar prioridades', error: error.message });
+        }
+    };
+
 
 };
 
